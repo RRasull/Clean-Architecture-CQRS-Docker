3b3deb8d9599899430468500d3bdfe5a1910ca5e
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-19 03:17:38 +0400

    'refactor: modify GetAllProductQueryHandler'

--- Clean-Architecture-CQRS-Docker.Application/CQRS/Handlers/QueryHandlers/GetAllProductQueryHandler.cs
+++ Clean-Architecture-CQRS-Docker.Application/CQRS/Handlers/QueryHandlers/GetAllProductQueryHandler.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Clean_Architecture_CQRS_Docker.Application.CQRS.Queries.Request;
+using Clean_Architecture_CQRS_Docker.Application.CQRS.Queries.Response;
+using Clean_Architecture_CQRS_Docker.Domain.Entities;
+using Clean_Architecture_CQRS_Docker.Infrastructure.DAL;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,33 @@ using System.Threading.Tasks;
 
 namespace Clean_Architecture_CQRS_Docker.Application.CQRS.Handlers.QueryHandlers
 {
-    internal class GetAllProductQueryHandler
+    public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, List<GetAllProductQueryResponse>>
     {
+        private readonly AppDbContext _context;
+
+
+        public GetAllProductQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetAllProductQueryResponse>> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            var products = await _context.Products.ToListAsync();
+            List<GetAllProductQueryResponse> productsResponse = new List<GetAllProductQueryResponse>();
+
+            foreach (var item in products)
+            {
+                var model = new GetAllProductQueryResponse()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Price = item.Price
+                };
+                productsResponse.Add(model);
+            }
+
+            return productsResponse;
+        }
     }
 }
