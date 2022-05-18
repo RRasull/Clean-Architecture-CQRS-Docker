dc1b41d5fff444a5ccb659873f3a7524afa7ff5e
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-19 02:40:34 +0400

    Add project files.

---
+++ Clean-Architecture-CQRS-Docker/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clean_Architecture_CQRS_Docker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
