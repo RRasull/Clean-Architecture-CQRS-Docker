130461d1b2c1936d5d8014590e1fa0624a27568e
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-19 02:47:44 +0400

    'feat: add GetAllProductQuery'

---
+++ Clean-Architecture-CQRS-Docker.Application/CQRS/Queries/Request/GetAllProductQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using Clean_Architecture_CQRS_Docker.Application.CQRS.Queries.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Architecture_CQRS_Docker.Application.CQRS.Queries.Request
+{
+    public class GetAllProductQueryRequest : IRequest<List<GetAllProductQueryResponse>>
+    {
+    }
+}
---
+++ Clean-Architecture-CQRS-Docker.Application/CQRS/Queries/Response/GetAllProductQueryResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Architecture_CQRS_Docker.Application.CQRS.Queries.Response
+{
+    public class GetAllProductQueryResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
