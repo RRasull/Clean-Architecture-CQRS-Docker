e3c186b84ab53c862dbb8e76c9ee5b6d7445fc11
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-19 11:09:58 +0400

    'feat: add CreateProductCommand'

---
+++ Clean-Architecture-CQRS-Docker.Application/CQRS/Commands/Request/CreateProductCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿using Clean_Architecture_CQRS_Docker.Application.CQRS.Commands.Response;
+using MediatR;
+
+namespace Clean_Architecture_CQRS_Docker.Application.CQRS.Commands.Request;
+
+public class CreateProductCommandRequest : IRequest<CreateProductCommandResponse>
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Clean-Architecture-CQRS-Docker.Application/CQRS/Commands/Response/CreateProductCommandResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Clean_Architecture_CQRS_Docker.Application.CQRS.Commands.Response;
+
+public class CreateProductCommandResponse
+{
+    public bool IsSuccess { get; set; }
+    public int ProductId { get; set; }
+}
