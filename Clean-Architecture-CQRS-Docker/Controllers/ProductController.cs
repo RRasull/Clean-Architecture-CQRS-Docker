f72711176bb20b0c2a918eeffe996e16b494fb36
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-19 11:19:12 +0400

    'feat: add HTTP Requests to ProductController'

--- Clean-Architecture-CQRS-Docker/Controllers/ProductController.cs
+++ Clean-Architecture-CQRS-Docker/Controllers/ProductController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Clean_Architecture_CQRS_Docker.Application.CQRS.Commands.Request;
+using Clean_Architecture_CQRS_Docker.Application.CQRS.Commands.Response;
+using Clean_Architecture_CQRS_Docker.Application.CQRS.Queries.Request;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Clean_Architecture_CQRS_Docker.Controllers
@@ -7,6 +11,39 @@ namespace Clean_Architecture_CQRS_Docker.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IMediator _mediatR;
 
+        public ProductController(IMediator mediatR)
+        {
+            _mediatR = mediatR;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var query = new GetByIdProductQueryRequest() { Id = id };
+            return Ok(await _mediatR.Send(query));
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllProductQueryRequest();
+            return Ok(await _mediatR.Send(query));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductCommandRequest requestModel)
+        {
+            CreateProductCommandResponse response = await _mediatR.Send(requestModel);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var query = new DeleteProductCommandRequest() { Id = id };
+            return Ok(await _mediatR.Send(query));
+        }
     }
 }
