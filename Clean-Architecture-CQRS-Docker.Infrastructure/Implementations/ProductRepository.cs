3393c3098b1fe260409ed0196defaa16f92fce02
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-19 12:39:30 +0400

    'feat: add Repository.cs and ProductRepository.cs'

---
+++ Clean-Architecture-CQRS-Docker.Infrastructure/Implementations/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Clean_Architecture_CQRS_Docker.Domain.Entities;
+using Clean_Architecture_CQRS_Docker.Domain.Interfaces;
+using Clean_Architecture_CQRS_Docker.Infrastructure.DAL;
+
+namespace Clean_Architecture_CQRS_Docker.Infrastructure.Implementations;
+
+public class ProductRepository : Repository<Product>, IProductRepository
+{
+    private AppDbContext _context { get; }
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
